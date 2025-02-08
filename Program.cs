0232dd928c0f991a2ffa2bdd36a1ae29451309d7
jakubp <jakubp@jpaudio.org>
2025-02-08 00:36:26 -0800

    implemented first iteration of week grid display

--- Program.cs
+++ Program.cs
@@ -1,22 +1,25 @@
 ﻿string[] habitsToTrack = ["Meditate", "Read", "Walk", "Code"];
+DayOfWeek[] daysOfWeek = new DayOfWeek[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday, DayOfWeek.Saturday, DayOfWeek.Sunday };
+var dt = DateTime.Now;
 
 // User input helper vriables
 string? userInput;
 bool exit = false;
 
-
 do
 {
-    Console.WriteLine("Welcome to habit tracker");
+    Console.WriteLine($"\n\t\tWelcome to Habit Tracker. Current date is {dt.DayOfWeek} {DateOnly.FromDateTime(dt)}.\n");
     // Present menu options
     // Show custom date
+    ShowWeekGrid();
     // Add habit to list
     // Remove habit from list
     // Mark habit as completed
 
+    Console.WriteLine();
 
     userInput = Console.ReadLine();
-    if (userInput.ToLower().Contains("exit"))
+    if (userInput != null && userInput.ToLower().Contains("exit"))
     {
         exit = true;
     }
@@ -25,7 +28,22 @@ do
     // Display current / selected week habit streak na all time best streak
 
 
-
 }
 while (exit == false);
 
+void ShowWeekGrid()
+{
+    string weekGrid = "";
+
+    foreach (var day in daysOfWeek)
+    {
+        string currentWeekDay = $"{day.ToString()} ";
+        weekGrid += currentWeekDay;
+    }
+    Console.WriteLine($"\t\t{weekGrid}\n");
+
+    foreach (string habit in habitsToTrack)
+    {
+        Console.WriteLine($"{habit}\n");
+    }
+}
