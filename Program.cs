3bd268bc6fed5bcd5f9acdd400a1fb278bdc1401
jakubp <jakubp@jpaudio.org>
2025-02-07 22:09:39 -0800

    added prototype data and basic program loop with exit opttion

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿string[] habitsToTrack = ["Meditate", "Read", "Walk", "Code"];
+
+// User input helper vriables
+string? userInput;
+bool exit = false;
+
+
+do
+{
+    Console.WriteLine("Welcome to habit tracker");
+    userInput = Console.ReadLine();
+    if (userInput.ToLower().Contains("exit"))
+    {
+        exit = true;
+    }
+    // Disaplay current week and habit status
+
+}
+while (exit == false);
+
