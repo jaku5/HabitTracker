c4e61d26f06dda75343aed0160cf9ad1f16fe1fc
jakubp <jakubp@jpaudio.org>
2025-02-07 22:17:13 -0800

    added pseudo code with planned featues

--- Program.cs
+++ Program.cs
@@ -8,12 +8,23 @@ bool exit = false;
 do
 {
     Console.WriteLine("Welcome to habit tracker");
+    // Present menu options
+    // Show custom date
+    // Add habit to list
+    // Remove habit from list
+    // Mark habit as completed
+
+
     userInput = Console.ReadLine();
     if (userInput.ToLower().Contains("exit"))
     {
         exit = true;
     }
+
     // Disaplay current week and habit status
+    // Display current / selected week habit streak na all time best streak
+
+
 
 }
 while (exit == false);
